c3337066173cf1c30842f682926a6a3e9d6afbb1
aryan595 <155081740+aryan595@users.noreply.github.com>
2024-09-20 10:28:34 +0530

    Add files via upload
    Some of my basic projects.

---
+++ Dice Roller Program/script.js
@@ -0,0 +1,17 @@
+function Roll() {
+
+    const numofdice = document.getElementById("numofdice").value;
+    const diceresult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numofdice; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="Dice images/${value}.png" alt="Dice ${value}">`);
+    }
+
+    diceresult.textContent = `Dice: ${values.join(", ")}`;
+    diceImages.innerHTML = images.join('')
+}
